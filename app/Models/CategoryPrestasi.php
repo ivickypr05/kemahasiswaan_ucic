e23da1e5c58052c5b541f64959bb05b44cf6ce1c
ivickypr05 <iko.pratama.ti.20@cic.ac.id>
2023-07-15 21:23:22 +0700

    add relations prestasi with category prestasi

--- app/Models/CategoryPrestasi.php
+++ app/Models/CategoryPrestasi.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class CategoryPrestasi extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'nama'
+    ];
+    public function prestasiindividu(): HasMany
+    {
+        return $this->hasMany(PrestasiIndividu::class, 'category_prestasi_id', 'id');
+    }
+    public function prestasitim(): HasMany
+    {
+        return $this->hasMany(PrestasiTim::class, 'category_prestasi_id', 'id');
+    }
 }
