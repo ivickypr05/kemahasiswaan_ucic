723108a2ad09f65f2c635dd02e0a99259308457c
Ahmad Alif <ahmadaliffauzan@gmail.com>
2023-08-01 02:04:11 +0700

    update view layout berita

--- resources/views/frontend/berita/berita.blade.php
+++ resources/views/frontend/berita/berita.blade.php
@@ -2,23 +2,36 @@
 
 @section('style-fe')
     <style>
-        .square-image {
-            width: 100%;
-            height: 0;
-            padding-bottom: 100%;
-            position: relative;
-            overflow: hidden;
+        .box {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
         }
 
-        .square-image img {
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            width: auto;
+        .box > div {
+            width: 100%;
+        }
+
+        .box > div:last-child {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+
+        .square-image {
+            width: 150px;
+            height: 150px;
+            overflow: hidden;
+            border-radius: 5px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .small-square-img {
+            width: 100%;
             height: 100%;
             object-fit: cover;
-            object-position: center center;
         }
 
         .beasiswa-info {
@@ -42,7 +55,7 @@
         }
 
         /* Tambahkan margin-top pada section#about */
-        section#about {
+        section#pricing {
             margin-top: 100px;
         }
     </style>
@@ -50,39 +63,42 @@
 
 @section('content-fe')
     @forelse ($berita as $item)
-        <section id="about" class="about mb-5">
-            <div class="container">
+    <section id="pricing" class="pricing">
+  <div class="container">
 
-                <div class="row content">
-                    <div class="col-lg-6">
-                    <div class="square-image">
-                            <img src="{{ asset('storage/' . ($item->gambar ?? 'https://c4.wallpaperflare.com/wallpaper/94/602/369/surface-light-silver-background-wallpaper-preview.jpg')) }}"
-                                alt="">
-                        </div>
-                    </div>
-                    <div class="col-lg-6 pt-4 pt-lg-0" style="padding-top: 2%!important;">
-                        <div class="beasiswa-info">
-                            <h4 class="beasiswa-title">{{ $item->title }}</h4>
-                            <div class="beasiswa-time">
-                                <i class="bi bi-clock"></i>
-                                <p>{{ \Carbon\Carbon::parse($item->dari_tanggal)->translatedFormat('d F Y') }}
-                                    - {{ \Carbon\Carbon::parse($item->sampai_tanggal)->translatedFormat('d F Y') }}</p>
-                            </div>
-                        </div>
-                        @php
-                            $limitedContent = Str::limit($item->content, 950);
-                            $formattedContent = nl2br($limitedContent);
-                        @endphp
-                        <p class="mt-2" style="text-align: justify;">{!! $formattedContent !!}</p>
-                        @if (strlen($item->content) > 200)
-                            <a href="{{ route('detail-berita', $item->id) }}"
-                                class="btn btn-primary" style="float:right">Read More</a>
-                        @endif
-                    </div>
-                </div>
-
+    <div class="row">
+    <div class="col-lg-12">
+        <center><h5><b>Halaman Berita</b></h5></center>
+        <br><br>
+      </div>
+      @foreach ($berita->take(3) as $item)
+        <div class="col-lg-4 col-md-6">
+          <div class="box">
+            <div>
+              <h5 class="card-title"><b>{{ $item->title }}</b></h5>
+              <p>{{ \Carbon\Carbon::parse($item->dari_tanggal)->translatedFormat('d F Y') }} - {{ \Carbon\Carbon::parse($item->sampai_tanggal)->translatedFormat('d F Y') }}</p>
             </div>
-        </section><!-- End About Section -->
+
+            <div class="square-image">
+              <img class="small-square-img" src="{{ asset('storage/' . ($item->gambar ?? 'https://c4.wallpaperflare.com/wallpaper/94/602/369/surface-light-silver-background-wallpaper-preview.jpg')) }}"
+                alt="">
+            </div>
+
+            @php
+            $limitedContent = Str::limit($item->content, 300);
+            $formattedContent = nl2br($limitedContent);
+            @endphp
+            <p class="mt-2" style="text-align: justify;">{!! $formattedContent !!}</p>
+            @if (strlen($item->content) > 200)
+              <a href="{{ route('detail-berita', $item->id) }}" class="btn btn-primary">Read More</a>
+            @endif
+          </div>
+        </div>
+      @endforeach
+    </div>
+
+  </div>
+</section>
     @empty
         <section id="about" class="about mb-5">
             <div class="container">
