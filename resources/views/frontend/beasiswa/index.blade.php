8fdeed253cd704ac2693906b71a3b309ee91b920
Ahmad Alif <ahmadaliffauzan@gmail.com>
2023-08-01 22:10:07 +0700

    update layouting beasiswa

--- resources/views/frontend/beasiswa/index.blade.php
+++ resources/views/frontend/beasiswa/index.blade.php
@@ -43,7 +43,7 @@
 
         /* Tambahkan margin-top pada section#about */
         section#pricing {
-            margin-top: 10px;
+            margin-top: 100px;
         }
     </style>
 @endsection
@@ -51,56 +51,52 @@
     <h1>Beasiswa Universitas Catur Insan Cendekia</h1>
 </div>
 @section('content-fe')
-    <section id="pricing" class="pricing">
-        <div class="container">
-            @forelse ($beasiswa->chunk(2) as $chunk)
+    @forelse ($beasiswa as $item)
+        <section id="about" class="about mb-3">
+            <div class="container">
+
                 <div class="row content">
-                    @foreach ($chunk as $item)
-                        <div class="col-lg-6 col-md-6">
-                            <div class="box" style="margin-top: 20px;">
-                                <div class="square-image">
-                                    <img src="{{ asset('storage/' . ($item->gambar ?? 'https://c4.wallpaperflare.com/wallpaper/94/602/369/surface-light-silver-background-wallpaper-preview.jpg')) }}"
-                                        alt="">
-                                </div>
-                                <br>
-                                <div class="beasiswa-info">
-                                    <div class="beasiswa-title">
-                                        <h5 class="card-title"><b>{{ $item->title }}</b></h5>
-                                    </div>
-                                    <div class="beasiswa-time">
-                                        <i class="fa fa-calendar"></i>
-                                        <p>{{ \Carbon\Carbon::parse($item->dari_tanggal)->translatedFormat('d F Y') }} -
-                                            {{ \Carbon\Carbon::parse($item->sampai_tanggal)->translatedFormat('d F Y') }}
-                                        </p>
-                                    </div>
-                                </div>
-                                @php
-                                    $limitedContent = Str::limit($item->content, 300);
-                                    $formattedContent = nl2br($limitedContent);
-                                @endphp
-                                <p class="mt-2" style="text-align: justify;">{!! $formattedContent !!}</p>
-                                @if (strlen($item->content) > 200)
-                                    <a href="{{ route('detail-beasiswa', $item->id) }}" class="btn btn-primary">Read
-                                        More</a>
-                                @endif
-                            </div>
-                        </div>
-                    @endforeach
-                </div>
-            @empty
-                <section id="pricing" class="pricing mb-5">
-                    <div class="container">
-                        <div class="row content">
-                            <div class="col-lg-12">
-                                <br></br>
-                                <h4 class="text-center text-primary">Belum ada informasi Beasiswa.</h4>
-                            </div>
+                    <div class="col-lg-6">
+                        <div class="square-image">
+                            <img src="{{ asset('storage/' . ($item->gambar ?? 'https://c4.wallpaperflare.com/wallpaper/94/602/369/surface-light-silver-background-wallpaper-preview.jpg')) }}"
+                                alt="">
                         </div>
                     </div>
-                </section><!-- End About Section -->
-            @endforelse
-        </div>
-    </section><!-- End About Section -->
+                    <div class="col-lg-6 pt-4 pt-lg-0" style="padding-top: 10%!important;">
+                        <div class="beasiswa-info">
+                            <h4 class="beasiswa-title">{{ $item->title }}</h4>
+                            <div class="beasiswa-time">
+                                <i class="bi bi-clock"></i>
+                                <p>{{ \Carbon\Carbon::parse($item->dari_tanggal)->translatedFormat('d F Y') }}
+                                    - {{ \Carbon\Carbon::parse($item->sampai_tanggal)->translatedFormat('d F Y') }}</p>
+                            </div>
+                        </div>
+                        @php
+                            $limitedContent = Str::limit($item->content, 950);
+                            $formattedContent = nl2br($limitedContent);
+                        @endphp
+                        <p class="mt-2" style="text-align: justify;">{!! $formattedContent !!}</p>
+                        @if (strlen($item->content) > 200)
+                            <a href="{{ route('detail-beasiswa', $item->id) }}" class="btn btn-primary" style="float:right">Read
+                                More</a>
+                        @endif
+                    </div>
+
+                </div>
+        </section><!-- End About Section -->
+    @empty
+        <section id="pricing" class="pricing mb-5">
+            <div class="container">
+                <div class="row content">
+                    <div class="col-lg-12">
+                        <br></br>
+                        <br></br>
+                        <h4 class="text-center text-primary">Belum ada informasi PKM.</h4>
+                    </div>
+                </div>
+            </div>
+        </section><!-- End About Section -->
+    @endforelse
 @endsection
 
 @section('script-fe')
