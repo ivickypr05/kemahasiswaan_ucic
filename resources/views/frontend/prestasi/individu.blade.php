810b7840b0cede5fb28a0fb0216a45784a2e79ea
ivickypr05 <iko.pratama.ti.20@cic.ac.id>
2023-08-01 14:06:14 +0700

    styling prestasi individu

--- resources/views/frontend/prestasi/individu.blade.php
+++ resources/views/frontend/prestasi/individu.blade.php
@@ -2,24 +2,15 @@
 
 @section('style-fe')
     <style>
-        .square-image {
-            width: 100%;
-            height: 0;
-            padding-bottom: 100%;
-            position: relative;
-            overflow: hidden;
+        .card-img-tip {
+            max-width: 100%;
+            height: 100%;
+            display: block;
+            margin: 0 auto;
+            /* untuk mengatur gambar menjadi tengah */
+
         }
 
-        .square-image img {
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            width: auto;
-            height: 100%;
-            object-fit: cover;
-            object-position: center center;
-        }
 
         .beasiswa-info {
             display: flex;
@@ -45,52 +36,98 @@
         section#pricing {
             margin-top: 100px;
         }
+
+        .card {
+            font-family: 'Familjen Grotesk', sans-serif;
+
+
+            &:hover,
+            &:focus-within {
+                transform: translatey(-3px);
+                box-shadow: 0 1rem 1rem rgb(0, 0, 0);
+                transform: translateY(-1rem);
+                backdrop-filter: blur(1.5rem);
+            }
+
+        }
+
+        .card-body .h6 {
+            position: relative;
+            width: 222px;
+            height: 500px;
+            background: rgba(255, 255, 255, 0.05);
+            margin: 5px;
+            box-shadow: 0 10px 35px rgba(0, 0, 0, 0.5);
+            border-radius: 8px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            backdrop-filter: blur(10px);
+            font-size: 20px;
+        }
     </style>
 @endsection
 <div class="container d-flex justify-content-center" style="padding-top: 10%!important;">
     <h1>Prestasi Individu Mahasiswa UCIC</h1>
 </div>
-
 @section('content-fe')
-    @forelse ($preindividu as $item)
-        <section id="about" class="about mb-5">
-            <div class="container">
+    <section id="about" class="about">
+        <div class="container">
+            <div class="row content d-flex justify-content-center">
+                @forelse ($preindividu as $item)
+                    <div class="mt-1 mb-5 col-md-4">
+                        {{-- card --}}
+                        <div class="card card-deck">
+                            <img src="{{ asset('storage/' . $item->gambar_1) }}" class="card-img-tip" width="150px"
+                                alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title text-center"><strong>{{ $item->title }}</strong></h5>
+                                <hr>
+                                <h6 class="card-text"> Tingkat :
+                                    {{ $item->tingkat_kejuaraan }} </h6>
+                                @if ($item->categories == null)
+                                    <h6 class=" text-muted"><i>tidak ada
+                                            kategori</i></h6>
+                                @else
+                                    <h6 class="text-muted"><i>Kategori :
+                                            {{ $item->categories->nama }}</i>
+                                    </h6>
+                                @endif
+                                <h6 class=" text-muted d-flex justify-content-end">Tanggal :
+                                    {{ \Carbon\Carbon::parse($item->tanggal)->translatedFormat('d F Y') }} </h6>
+                                <br>
+                                <h6 class="card-text"><strong>Nama Peserta : {{ $item->nama }} </strong></h6>
 
-                <div class="row content">
-                    <div class="col-lg-6">
-                        <img style="width:100%"
-                            src="{{ asset('storage/' . ($item->gambar_1 ?? 'https://c4.wallpaperflare.com/wallpaper/94/602/369/surface-light-silver-background-wallpaper-preview.jpg')) }} "
-                            alt="">
-                    </div>
-                    <div class="col-lg-6 pt-4 pt-lg-0" style="padding-top: 10%!important;">
-                        <div class="row">
-                            <div class="left" style="float:left">
-                                <h4> <b>{{ $item->title }}</b> </h4>
-                            </div>
-                            <div class="right" style="float:right">
-                                <p style="float:right">
-                                    {{ \Carbon\Carbon::parse($item->dari_tanggal)->translatedFormat('d F Y') }} -
-                                    {{ \Carbon\Carbon::parse($item->sampai_tanggal)->translatedFormat('d F Y') }} </p>
+                                @php
+                                    $limitedContent = Str::limit($item->deskripsi, 100);
+                                    $formattedContent = nl2br($limitedContent);
+                                @endphp
+                                <p class="mt-2" style="text-align: justify;">{!! $formattedContent !!}</p>
+                                @if (strlen($item->deskripsi) > 100)
+                                    <a href="{{ route('detail-bkm', $item->id) }}" class="btn btn-primary"
+                                        style="float:right">Detail</a>
+                                @endif
+
                             </div>
                         </div>
-                        <p class="mt-2">{{ $item->deskripsi }}</p>
+
                     </div>
-                </div>
+                @empty
+                    <section id="pricing" class="pricing mb-5">
+                        <div class="container">
+                            <div class="row content">
+                                <div class="col-lg-12">
+                                    <br></br>
+                                    <br></br>
+                                    <h4 class="text-center text-primary">Belum ada informasi data prestasi Individu.</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </section><!-- End About Section -->
 
             </div>
-        </section><!-- End About Section -->
-    @empty
-        <section id="pricing" class="pricing mb-5">
-            <div class="container">
-                <div class="row content">
-                    <div class="col-lg-12">
-                        <br></br>
-                        <br></br>
-                        <h4 class="text-center text-primary">Belum ada informasi data prestasi Individu.</h4>
-                    </div>
-                </div>
-            </div>
-        </section><!-- End About Section -->
+        </div>
+    </section><!-- End About Section -->
     @endforelse
 @endsection
 
