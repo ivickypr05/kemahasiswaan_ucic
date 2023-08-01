f18eb171221acf6e34b97d81f15ed9b351e217c6
Ahmad Alif <ahmadaliffauzan@gmail.com>
2023-08-01 23:02:31 +0700

    add content in footer

--- resources/views/layouts-fe/partials/footer.blade.php
+++ resources/views/layouts-fe/partials/footer.blade.php
@@ -1,74 +1,84 @@
 <footer id="footer" style="background-color: #0F2B5B;">
-    <div class="footer-top" style="background-color: #1C3F94;">
-      <div class="container">
-        <div class="row">
+  <div class="footer-top" style="background-color: #1C3F94;">
+    <div class="container">
+      <div class="row">
 
-          <div class="col-lg-3 col-md-6">
-            <div class="footer-info" >
-              <h3>UNIVERSITAS CATUR INSAN CENDEKIA</h3>
-              <p>
+        <div class="col-lg-3 col-md-6">
+          <div class="footer-info">
+            <h3>KEMAHASISWAAN UNIVERSITAS CATUR INSAN CENDEKIA</h3>
+            <p>
+            Kami berkomitmen untuk menyediakan akses <br>
+            informasi yang bermanfaat bagi seluruh mahasiswa.<br>
+            Temukan berbagai informasi tentang beasiswa, <br>
+            prestasi, organisasi, simbelmawa, dan berita terkini di sini.
+            </p>
+            <div class="social-links mt-3">
+              <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+              <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+              <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+              <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+              <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-2 col-md-6">
+          <div class="footer-links">
+            <h4>Fitur</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Informasi Beasiswa</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Informasi Prestasi</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Informasi Kegiatan Organisasi</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Informasi SIMBELMAWA</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Informasi Berita</a></li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6">
+          <div class="footer-links">
+            <h4>Kontak</h4>
+            <p>
+              <strong>Phone:</strong> 0231-200418<br>
+              <strong>Email:</strong> stmik.cic.cirebon@gmail.com<br>
+            </p><br>
+            <h4>Alamat</h4>
+            <p>
               Jl. Kesambi No.202, Drajat, Kec. Kesambi, <br>
               Kota Cirebon, Jawa Barat 45133<br><br>
-                <strong>Phone:</strong> 0231-200418<br>
-                <strong>Email:</strong> stmik.cic.cirebon@gmail.com<br>
-              </p>
-              <div class="social-links mt-3">
-                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-                <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-                <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-              </div>
-            </div>
+              
+            </p>
+          </div>
         </div>
+
+        <div class="col-lg-3 col-md-6">
+                    <iframe style="border:0; width: 150%; height: 250px;"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3962.296980321091!2d108.55121841424463!3d-6.733576795132232!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6f1d8ebc133e3f%3A0x91385801f5822049!2sUNIVERSITAS%20CIC!5e0!3m2!1sen!2sid!4v1677997552754!5m2!1sen!2sid"
+                        frameborder="0" allowfullscreen></iframe>
+                </div>
+
       </div>
-          <!-- <div class="col-lg-2 col-md-6 footer-links">
-            <h4>Useful Links</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
-            </ul>
-          </div> -->
+    </div>
+  </div>
 
-          <!-- <div class="col-lg-3 col-md-6 footer-links">
-            <h4>Our Services</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
-            </ul>
-          </div> -->
-
-          <!-- <div class="col-lg-4 col-md-6 footer-newsletter">
-            <h4>Our Newsletter</h4>
-            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
-            <form action="" method="post">
-              <input type="email" name="email"><input type="submit" value="Subscribe">
-            </form>
-
-          </div> -->
-
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="text-center">
+          <div class="credits">
+            <!-- All the links in the footer should remain intact. -->
+            <!-- You can delete the links only if you purchased the pro version. -->
+            <!-- Licensing information: https://bootstrapmade.com/license/ -->
+            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/sailor-free-bootstrap-theme/ -->
+            <!-- Designed by <a href="#">Jeand Dev</a> -->
+          </div>
+          <div class="copyright">
+            <script>document.write(new Date().getFullYear())</script>
+            &copy; Copyright <strong><span>Kemahasiswaan UCIC</span></strong>. All Rights Reserved
+          </div>
         </div>
       </div>
     </div>
+  </div>
 
-    <div class="container">
-      <div class="copyright">
-        <script>document.write(new Date().getFullYear())</script>
-        &copy; Copyright <strong><span>Kemahasiswaan</span></strong>. All Rights Reserved
-      </div>
-      <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/sailor-free-bootstrap-theme/ -->
-        Designed by <a href="#">Jeand Dev</a>
-      </div>
-    </div>
-  </footer>
-</body>
+</footer>
