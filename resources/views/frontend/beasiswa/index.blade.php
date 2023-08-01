7044a70ee37f5422c2b38987f64587fb16df866f
Ahmad Alif <ahmadaliffauzan@gmail.com>
2023-08-01 10:22:40 +0700

    update layout beasiswa

--- resources/views/frontend/beasiswa/index.blade.php
+++ resources/views/frontend/beasiswa/index.blade.php
@@ -42,50 +42,46 @@
         }
 
         /* Tambahkan margin-top pada section#about */
-        section#about {
+        section#pricing {
             margin-top: 100px;
         }
     </style>
 @endsection
 
 @section('content-fe')
-    @forelse ($beasiswa as $item)
-        <section id="about" class="about mb-5">
-            <div class="container">
-
+    <section id="pricing" class="pricing">
+        <div class="container">
+            @forelse ($beasiswa->chunk(2) as $chunk)
                 <div class="row content">
-                    <div class="col-lg-6">
-                        <div class="square-image">
-                            <img src="{{ asset('storage/' . ($item->gambar ?? 'https://c4.wallpaperflare.com/wallpaper/94/602/369/surface-light-silver-background-wallpaper-preview.jpg')) }}"
-                                alt="">
-                        </div>
-                    </div>
-                    <div class="col-lg-6 pt-4 pt-lg-0" style="padding-top: 10%!important;">
-                        <div class="beasiswa-info">
-                            <h4 class="beasiswa-title">{{ $item->title }}</h4>
-                            <div class="beasiswa-time">
-                                <i class="bi bi-clock"></i>
-                                <p>{{ \Carbon\Carbon::parse($item->dari_tanggal)->translatedFormat('d F Y') }}
-                                    - {{ \Carbon\Carbon::parse($item->sampai_tanggal)->translatedFormat('d F Y') }}</p>
+                    @foreach ($chunk as $item)
+                        <div class="col-lg-6 col-md-6">
+                            <div class="box">
+                                <div class="square-image">
+                                    <img src="{{ asset('storage/' . ($item->gambar ?? 'https://c4.wallpaperflare.com/wallpaper/94/602/369/surface-light-silver-background-wallpaper-preview.jpg')) }}" alt="">
+                                </div>
+                                <br>
+                                <div class="beasiswa-info">
+                                    <div class="beasiswa-title">
+                                        <h5 class="card-title"><b>{{ $item->title }}</b></h5>
+                                    </div>
+                                    <div class="beasiswa-time">
+                                        <i class="fa fa-calendar"></i>
+                                        <p>{{ \Carbon\Carbon::parse($item->dari_tanggal)->translatedFormat('d F Y') }} - {{ \Carbon\Carbon::parse($item->sampai_tanggal)->translatedFormat('d F Y') }}</p>
+                                    </div>
+                                </div>
+                                @php
+                                    $limitedContent = Str::limit($item->content, 300);
+                                    $formattedContent = nl2br($limitedContent);
+                                @endphp
+                                <p class="mt-2" style="text-align: justify;">{!! $formattedContent !!}</p>
+                                @if (strlen($item->content) > 200)
+                                    <a href="{{ route('detail-beasiswa', $item->id) }}" class="btn btn-primary">Read More</a>
+                                @endif
                             </div>
                         </div>
-                        @php
-                            $limitedContent = Str::limit($item->content, 950);
-                            $formattedContent = nl2br($limitedContent);
-                        @endphp
-                        <p class="mt-2" style="text-align: justify;">{!! $formattedContent !!}</p>
-                        @if (strlen($item->content) > 200)
-                            <a href="{{ route('detail-beasiswa', $item->id) }}" class="btn btn-primary"
-                                style="float:right">Read More</a>
-                        @endif
-                    </div>
+                    @endforeach
                 </div>
-
-            </div>
-        </section><!-- End About Section -->
-    @empty
-        <section id="about" class="about mb-5">
-            <div class="container">
+            @empty
                 <div class="row content">
                     <div class="col-lg-12">
                         <br></br>
@@ -94,9 +90,9 @@
                         <h4 class="text-center">Belum ada informasi beasiswa.</h4>
                     </div>
                 </div>
-            </div>
-        </section><!-- End About Section -->
-    @endforelse
+            @endforelse
+        </div>
+    </section><!-- End About Section -->
 @endsection
 
 @section('script-fe')
